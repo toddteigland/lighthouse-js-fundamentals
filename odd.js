b107c68efe17ef3cda88012b69860629931d38aa
Todd <toddteigland@gmail.com>
2022-10-31 12:42:40 -0700

    odd/even functions

---
+++ odd.js
@@ -0,0 +1,10 @@
+let isOdd = function(num) {
+  if(num % 2 === 0) {
+    return true;
+  }else {
+    return false;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("3 is odd: " + isOdd(8));
