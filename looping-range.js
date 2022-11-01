5558c0d16c889a82cc6bb54112062d140fbd6f62
Todd <toddteigland@gmail.com>
2022-10-31 17:00:59 -0700

    looping through ranges excercise

---
+++ looping-range.js
@@ -0,0 +1,20 @@
+function range (start, end, step) {
+ if (start < end && step > 0) {
+  let output = [];
+  for (let i = start; i <= end; i += step) {
+    output.push(i);
+  }
+  return output;
+
+ }  else if ((start === undefined) || (end === undefined) || (end === undefined)) {
+    const noWorky = [];
+   return noWorky;
+
+  } else if (step <= 0 || start > end) {
+  const noWorky = [];
+  return noWorky;
+  }}
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
